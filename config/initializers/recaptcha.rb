f8d8aa647e554da69ef7d852981748613c1c4bea
Juan Robles <juan.robles@sage.com>
2013-08-10 16:30:50 -0400

    Initial version

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,4 @@
+Recaptcha.configure do |config|
+  config.public_key  = 'Get your keys here: http://www.google.com/recaptcha/whyrecaptcha'
+  config.private_key = 'Get your keys here: http://www.google.com/recaptcha/whyrecaptcha'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recaptcha::Application.config.secret_token = 'e9e6f400b631bfe71e99925e3ad35db6d1d31f0ba0611d74126f3cdb53bc2c54697caed0461bd1a27946928fd35d613332b1727c58cee64d5c19fcff762fff7f'
